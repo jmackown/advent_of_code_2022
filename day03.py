56eae14740f733f8ad80969c0c67d49fadca1d1e
jcmc <heyjcmc@gmail.com>
2022-12-04 08:44:19 +0000

    day 3 part 2

--- day03.py
+++ day03.py
@@ -1,41 +1,69 @@
 import string
 
-import pandas as pd
+import numpy as np
 
-from utilities.dataframes import get_input, load, display
+from utilities.dataframes import display, get_input, load
 
-
-PRIORITY_MAPPING = {c: i+1 for i, c in enumerate(list(string.ascii_lowercase) + list(string.ascii_uppercase))}
+PRIORITY_MAPPING = {
+    c: i + 1
+    for i, c in enumerate(list(string.ascii_lowercase) + list(string.ascii_uppercase))
+}
 
 
 def create_df(data=None):
     if not data:
         data = get_input(day="03")
     df = load(data)
-    df.rename(columns={0: 'contents'}, inplace=True)
+    df.rename(columns={0: "contents"}, inplace=True)
     return df
 
+
 def split_in_half(data):
-    halfway = int(len(data)/2)
+    halfway = int(len(data) / 2)
     return [data[:halfway], data[halfway:]]
+
 
 def get_common_elements(data):
 
     # strong assumption here that there will only be one common element
     return list(set(data[0]).intersection(data[1]))[0]
 
+
 def sum_priorities(df):
 
-
-
-    df['compartments'] = df.apply(lambda x: split_in_half(data=x['contents']), axis=1)
-    df['common'] = df.apply(lambda x: get_common_elements(data=x['compartments']), axis=1)
-    df['score'] = df['common'].map(PRIORITY_MAPPING)
+    df["compartments"] = df.apply(lambda x: split_in_half(data=x["contents"]), axis=1)
+    df["common"] = df.apply(
+        lambda x: get_common_elements(data=x["compartments"]), axis=1
+    )
+    df["score"] = df["common"].map(PRIORITY_MAPPING)
 
     display(df)
 
-    return df['score'].sum()
+    return df["score"].sum()
+
+def get_more_common_elements(x):
+    contents = x.to_list()
+    common = set.intersection(*map(set, contents))
+
+
+    return list(common)[0]
+
+
+def groups(df):
+    df['every_3rd'] = df[::3]
+    df['group'] = df['every_3rd'].notnull().cumsum()
+    df = df.groupby('group')['contents'].apply(get_more_common_elements).reset_index()
+
+    df["score"] = df["contents"].map(PRIORITY_MAPPING)
+
+    display(df)
+
+    return df["score"].sum()
 
 
 part1 = sum_priorities(df=create_df())
 print(f"Part 1: {part1}")
+
+
+part2 = groups(df=create_df())
+print(f"Part 2: {part2}")
--- tests/test_day_03.py
+++ tests/test_day_03.py
@@ -1,11 +1,16 @@
-from day03 import sum_priorities, create_df
+from day03 import create_df, sum_priorities, groups
 
 test_data = "./data/day_03.txt"
 test_df = create_df(data=test_data)
-
 
 
 def test_sum_priorities():
     result = sum_priorities(df=test_df)
 
     assert result == 157
+
+
+def test_groups():
+    result = groups(df=test_df)
+
+    assert result == 70
